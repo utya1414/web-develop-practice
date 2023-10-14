99ebe03565377158a4dea386fa00224b886048d7
utya1414 <utya1414@gmail.com>
2023-10-14 20:52:25 +0900

    import and use MUI

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,7 +1,7 @@
 import TodoList from "./TodoList";
 import { useState, useRef } from "react";
 import {v4 as uuidv4 } from "uuid";
-import convertLikedDescription from "./linked";
+import { Button, Input } from "@mui/material";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -29,37 +29,12 @@ function App() {
     const newTodos = todos.filter((todo) => !todo.completed);
     setTodos(newTodos);
   }
-
-  const descripition = "ああhttps://www.google.co.jp";
-
-  /*const linked = (description) => {
-    
-    /*const regexp_url = /https?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+/g;
-    const regexp_makeLink = (url) => {
-        return '<a href="' + url + '">' + url + '</a>';
-    }
-    const addBlank = (descripition) => {
-      return " " + descripition + " ";
-    }
-    const blankedDescription = description.replace(regexp_url, addBlank);
-    // const linkedDescription = descripition.replace(regexp_url, regexp_makeLink)
-    const linkedDescription = convertLikedDescription(descripition);
-    //console.log(typeof linkedDescription);
-    return (
-      //<p dangerouslySetInnerHTML={{__html: linkedDescription}}></p>
-      <>{linkedDescription}</>
-    )
-    ;
-  }*/
-  
-    // const linkedDescription = convertLikedDescription(descripition);
-  const linkedDescription = convertLikedDescription(descripition);
   return (
   <>
     <TodoList todos={todos} toggleTodo={toggleTodo}/>
-    <input type="text" ref={todoNameRef} toggleTodo={toggleTodo} />
-    <button onClick={handleAddTodo}>タスクを追加</button>
-    <button onClick={handleClear} >完了したタスクの消去</button>
+    <Input type="text" ref={todoNameRef} toggleTodo={toggleTodo} />
+    <Button variant="outlined" onClick={handleAddTodo}>タスクを追加</Button>
+    <Button variant="outlined" onClick={handleClear} >完了したタスクの消去</Button>
     <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
   </>
   );
